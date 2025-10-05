3eb279fb3dead86d8cb4b06597286cfbdcd46e85
Shreyas <c2825212004@gmail.com>
2025-10-05 11:39:12 +0000

    Why the message is still showing at the bottom of the page of the sign u

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -12,8 +12,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      position="top-center"
-      duration={5000}
       toastOptions={{
         classNames: {
           toast:
